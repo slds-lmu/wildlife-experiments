cf7644a601709559f5f5eef12c3b1e65bdf98572
lisa-wm <wml1293@outlook.de>
2022-07-21 10:16:21 +0200

    .

--- scripts/prep_data.py
+++ scripts/prep_data.py
@@ -18,14 +18,15 @@ shutil.copyfile(
 
 # Create label map and  label file with two columns (img key, numeric label)
 
-info_dict = {k: v for k, v in load_csv_dict(os.path.join(ROOT_DIR, INFO_FILE))}
-class_names = list(set([v['true_class'] for v in info_dict.values()]))
-class_names = sorted(class_names)
+info_list = load_csv_dict(os.path.join(ROOT_DIR, INFO_FILE))
 
+class_names = list(set([x['true_class'] for x in info_list]))
+class_names = sorted(class_names)
 label_map = {class_names[i]: i for i in range(len(class_names))}
-label_dict = {k: label_map[v['true_class']] for k, v in info_dict}
-save_as_json(label_map, os.path.join(ROOT_DIR, 'label_map.json'))
+label_dict = {x['orig_name']: label_map[x['true_class']] for x in info_list}
+
+save_as_json(label_map, os.path.join(TARGET_DIR, 'label_map.json'))
 save_as_csv(
     [(k, v) for k, v in label_dict.items()],
-    os.path.join(ROOT_DIR, 'labels.csv'),
+    os.path.join(TARGET_DIR, 'labels.csv'),
 )
