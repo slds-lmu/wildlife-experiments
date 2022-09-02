08787f2aa87a983d3e27d34e4d108ad23f750149
Lisa Wimmer <wml1293@outlook.de>
2022-09-02 15:12:39 +0200

    .

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,17 +1,18 @@
+#!/bin/bash
+
 GPUNAME=$1
-if [[ -z $GPUNAME ]];
+if [[ -z $GPUNAME ]]
 then
     echo `date`" - Missing mandatory arguments: GPU name. "
     exit 1
-elif [[ $ -eq 99]]
+elif [[ $1 -eq 99 ]]
 then
     export CUDA_VISIBLE_DEVICES=""
 else
     export CUDA_VISIBLE_DEVICES=$1
 fi
-
 SCRIPT=$2
-if [[ -z $SCRIPT ]];
+if [[ -z $SCRIPT ]]
 then
     echo `date`" - Missing mandatory arguments: script to run. "
     exit 1
