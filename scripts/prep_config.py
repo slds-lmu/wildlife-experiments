95ac4a47d4832d993fdff8c962db9f06584997bf
Lisa Wimmer <wml1293@outlook.de>
2022-11-03 09:59:03 +0100

    add omids optimal configs to cfg

--- scripts/prep_config.py
+++ scripts/prep_config.py
@@ -28,14 +28,14 @@ def main(repo_dir: str, root_dir: str, img_dir: str):
         'md_batchsize': 32,
         'random_state': 123,
         'splits': (0.7, 0.15, 0.15),
-        'batch_size': 64,
+        'batch_size': 50,
         'num_classes': 8,
-        'transfer_epochs': 1,
-        'finetune_epochs': 1,
+        'transfer_epochs': 10,
+        'finetune_epochs': 4,
         'finetune_layers': 1,
-        'transfer_learning_rate': 1e-3,
-        'finetune_learning_rate': 1e-3,
-        'model_backbone': 'resnet50',
+        'transfer_learning_rate': 1e-4,
+        'finetune_learning_rate': 5e-3,
+        'model_backbone': 'xception',
         'num_workers': 16,
         'eval_metrics': ['accuracy'],
         'test_logfile': 'test_logfile',
@@ -43,11 +43,6 @@ def main(repo_dir: str, root_dir: str, img_dir: str):
         'al_batchsize': 32,
         'al_iterations': 1,
         'pretraining_ckpt': 'pretrained_weights',
-        'n_trials': 5,
-        'time_budget': 360000,  # 100hours
-        'max_concurrent_trials': 4,
-        'search_alg_id': 'randomsearch',
-        'scheduler_alg_id': 'ashascheduler',
     }
     save_as_json(cfg, os.path.join(repo_dir, 'configs/cfg.json'))
 
