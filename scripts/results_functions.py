c425153001ae23458be05448e24a7fd28eefcc74
Omid Charrakh <omid.charrakh@gmail.com>
2022-11-29 15:34:24 +0100

    resolved hard-coding issue

--- scripts/results_functions.py
+++ scripts/results_functions.py
@@ -8,6 +8,7 @@ import seaborn as sns
 import itertools
 from wildlifeml.utils.datasets import map_bbox_to_img
 import cv2
+import random
 from sklearn.metrics import (
     accuracy_score,
     recall_score,
@@ -38,7 +39,7 @@ def build_df_pred(
         pred_class.append(pred_img.argmax())
         pred_score.append(round(pred_img.max(), 3))
         n_preds.append(len(ids_bbox))
-        pred_classes.append([format(x)for x in preds_bbox[ids_bbox].argmax(1)])
+        pred_classes.append([x for x in preds_bbox[ids_bbox].argmax(1)])
         pred_confs.append([round(x, 3) for x in preds_bbox[ids_bbox].max(1)])
         md_confs.append(
             [
@@ -47,7 +48,10 @@ def build_df_pred(
             ]
         )
         md_bboxs.append(
-            [detector_dict[k]['bbox'] for k in [keys_bbox[i] for i in ids_bbox]]
+            [
+                detector_dict[k]['bbox']
+                for k in [keys_bbox[i] for i in ids_bbox]
+            ]
         )
         all_scores.append([round(x, 3) for x in pred_img])
 
@@ -63,20 +67,23 @@ def build_df_pred(
     df['md_bboxs'] = md_bboxs
     df['all_scores'] = all_scores
     df['img_path'] = [os.path.join(image_data_dir, name) for name in img_name]
-    df['true_class'] = df['true_class'].astype('str')
-    df['pred_class'] = df['pred_class'].astype('str')
+    df['true_class'] = df['true_class'].astype(int)
+    df['pred_class'] = df['pred_class'].astype(int)
     return df
 
 
 def labelize_df_pred(
     df_pred: pd.DataFrame,
-    translator_dict: dict,
+    label_map: dict,
 ) -> pd.DataFrame:
 
+    inverse_map = {v: k for k, v in label_map.items()}
     df = df_pred.copy()
-    df['true_class'] = df['true_class'].map(translator_dict)
-    df['pred_class'] = df['pred_class'].map(translator_dict)
-    df['pred_classes'] = [[translator_dict[p] for p in ps] for ps in df['pred_classes']]
+    df['true_class'] = df['true_class'].map(inverse_map)
+    df['pred_class'] = df['pred_class'].map(inverse_map)
+    df['pred_classes'] = [
+        [inverse_map[p] for p in ps] for ps in df['pred_classes']
+        ]
     return df
 
 
@@ -139,60 +146,73 @@ def inspect_confusion(df_pred, normalize=True, labels=None, ax=None):
 def inspect_misclasses(
     df_pred: pd.DataFrame,
     test_label: str,
-    translator_dict: dict,
+    label_map: dict,
     n_displays: int,
-    descending: bool,
     is_truth: bool,
+    sorting='descending',
 ):
 
-    df_miss = df_pred[df_pred['true_class'] != df_pred['pred_class']]
-
+    # sorting can be: ['descending', 'ascending', 'random']
     if n_displays == 0:
-        print('No misclassification for the specified class :-)')
+        print('Please increase the number of displaying images (n_displays).')
         return
+
+    df_mis = df_pred[df_pred['true_class'] != df_pred['pred_class']]
+    if is_truth:
+        df_mis = df_mis[df_mis['true_class'] == test_label]
     else:
-        if is_truth:
-            df_miss = df_miss[df_miss['true_class'] == test_label]
-        else:
-            df_miss = df_miss[df_miss['pred_class'] == test_label]
-        df_miss = df_miss.reset_index(drop=True)
-        n_displays = min(n_displays, len(df_miss))
+        df_mis = df_mis[df_mis['pred_class'] == test_label]
+    df_mis = df_mis.sort_values(
+        by=['pred_score'], ascending=False, ignore_index=True,
+    )
 
-        for index in range(n_displays):
-            score_dict = dict(
-                zip(translator_dict.values(), df_miss.loc[index, 'all_scores'])
-            )
-            score_dict = dict(
-                sorted(score_dict.items(), key=lambda x: x[1], reverse=descending)
-            )
-            img_path = df_miss.loc[index, 'img_path']
-            md_confs = df_miss.loc[index, 'md_confs']
-            md_bboxs = df_miss.loc[index, 'md_bboxs']
-            n_preds = df_miss.loc[index, 'n_preds']
+    n_available = len(df_mis)
+    if n_displays > n_available:
+        print(f'There are {n_available} available images to be displayed.')
+    n_displays = min(n_displays, n_available)
+    all_ids = list(df_mis.index)
+    if sorting == 'ascending':
+        ids = all_ids[:n_displays]
+    elif sorting == 'descending':
+        ids = all_ids[:-n_displays-1:-1]
+    else:
+        ids = random.choices(all_ids, k=n_displays)
 
-            fig, ax = plt.subplots(figsize=(6, 6))
-            image = cv2.imread(img_path)
-            for index_bbox in range(n_preds):
-                md_bbox = md_bboxs[index_bbox]
-                md_conf = md_confs[index_bbox]
-                image = draw_bbox(bbox=md_bbox, image=image, text=str(md_conf))
-            ax.imshow(image)
-            plt.show()
-            plt.close()
-            print(f"img_name: {df_miss.loc[index, 'img_name']}")
-            print(f"true_class: {df_miss.loc[index, 'true_class']}")
-            print(f"pred_class: {df_miss.loc[index, 'pred_class']}")
-            print(f"pred_score: {df_miss.loc[index, 'pred_score']}")
-            print(f"n_preds: {df_miss.loc[index, 'n_preds']}")
-            print(f"pred_classes: {df_miss.loc[index, 'pred_classes']}")
-            print(f"pred_confs: {df_miss.loc[index, 'pred_confs']}")
-            print(f"md_confs: {df_miss.loc[index, 'md_confs']}")
-            print(f"score_dict: {score_dict}")
+    for index in ids:
+        score_dict = dict(
+            zip(label_map.keys(), df_mis.loc[index, 'all_scores'])
+        )
+        score_dict = dict(
+            sorted(score_dict.items(), key=lambda x: x[1], reverse=True)
+        )
+        img_path = df_mis.loc[index, 'img_path']
+        md_confs = df_mis.loc[index, 'md_confs']
+        md_bboxs = df_mis.loc[index, 'md_bboxs']
+        n_preds = df_mis.loc[index, 'n_preds']
+
+        fig, ax = plt.subplots(figsize=(6, 6))
+        image = cv2.imread(img_path)
+        for index_bbox in range(n_preds):
+            md_bbox = md_bboxs[index_bbox]
+            md_conf = md_confs[index_bbox]
+            image = draw_bbox(bbox=md_bbox, image=image, text=str(md_conf))
+        ax.imshow(image)
+        plt.show()
+        plt.close()
+        print(f"img_name: {df_mis.loc[index, 'img_name']}")
+        print(f"true_class: {df_mis.loc[index, 'true_class']}")
+        print(f"pred_class: {df_mis.loc[index, 'pred_class']}")
+        print(f"pred_score: {df_mis.loc[index, 'pred_score']}")
+        print(f"n_preds: {df_mis.loc[index, 'n_preds']}")
+        print(f"pred_classes: {df_mis.loc[index, 'pred_classes']}")
+        print(f"pred_confs: {df_mis.loc[index, 'pred_confs']}")
+        print(f"md_confs: {df_mis.loc[index, 'md_confs']}")
+        print(f"score_dict: {score_dict}")
 
 
-def plot_frequencies(df, labels, ax=None):
+def plot_frequencies(df, label_map, ax=None):
     sns.set_style("darkgrid")
-    frequencies = get_frequencies(df, labels, normalize=True)
+    frequencies = get_frequencies(df, label_map, normalize=True)
     if ax is None:
         fig, ax = plt.subplots(figsize=(4, 4))
     ax.pie(x=frequencies)
@@ -201,8 +221,9 @@ def plot_frequencies(df, labels, ax=None):
         plt.close()
 
 
-def plot_class_performance(df_pred, df_meta, labels, ax=None):
+def plot_class_performance(df_pred, df_meta, label_map, ax=None):
 
+    labels = list(label_map.keys())
     n_classes = len(labels)
     report = classification_report(
         y_true=df_pred['true_class'],
@@ -211,7 +232,7 @@ def plot_class_performance(df_pred, df_meta, labels, ax=None):
         zero_division=0,
         output_dict=True,
     )
-    frequencies = get_frequencies(df_meta, labels, normalize=True)
+    frequencies = get_frequencies(df_meta, label_map, normalize=True)
     precisions = [report[label]['precision'] for label in labels]
     recalls = [report[label]['recall'] for label in labels]
     f1s = [report[label]['f1-score'] for label in labels]
@@ -227,7 +248,7 @@ def plot_class_performance(df_pred, df_meta, labels, ax=None):
         label='frequency',
     )
     ax.bar(
-        x= x + 1 * width,
+        x=x + 1 * width,
         height=precisions,
         width=width,
         color='#165C8D',
@@ -254,8 +275,10 @@ def plot_class_performance(df_pred, df_meta, labels, ax=None):
         plt.close()
 
 
-def get_frequencies(df, labels, normalize=True):
-    frequencies = [Counter(df['true_class'])[label] for label in labels]
+def get_frequencies(df, label_map, normalize=True):
+    frequencies = [
+        Counter(df['true_class'])[label]for label in label_map.keys()
+    ]
     if normalize:
         frequencies = [freq/np.sum(frequencies) for freq in frequencies]
     return np.array(frequencies)
