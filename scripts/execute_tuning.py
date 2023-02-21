91712a60bbffef83063e40d0189b1c1930aadc8b
Lisa Wimmer <wml1293@outlook.de>
2023-02-21 09:07:02 +0100

    tuning rest

--- scripts/execute_tuning.py
+++ scripts/execute_tuning.py
@@ -81,9 +81,9 @@ def main(repo_dir: str):
 
     # Define search grid
     search_space: Dict = {
-        'model_backbone': ['densenet121'],  # ['xception', 'densenet121', 'inception_resnet_v2'],
-        'finetune_layers': [0, 0.05, 0.25, 0.5],
-        'md_conf': [0.1, 0.25, 0.5, 0.9]
+        'model_backbone': ['xception', 'densenet121'],  # ['xception', 'densenet121', 'inception_resnet_v2'],
+        'finetune_layers': [0.5],  # [0, 0.05, 0.25, 0.5],
+        'md_conf': [0.5, 0.9]  # [0.1, 0.25, 0.5, 0.9]
     }
     search_grid = list(product_dict(**search_space))
 
@@ -209,7 +209,7 @@ def main(repo_dir: str):
             ]
         )
         df = pd.DataFrame(tuning_archive, columns=col_names)
-        archive_file = os.path.join(cfg['result_dir'], 'results_tuning_archive_2.csv')
+        archive_file = os.path.join(cfg['result_dir'], 'results_tuning_archive.csv')
         if os.path.exists(archive_file):
             existing = pd.read_csv(archive_file, usecols=col_names)
             combined = pd.concat([existing, df], ignore_index=True)
