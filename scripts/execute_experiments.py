7b1480ea458ab680930ec969f8ef722b225799f4
Lisa Wimmer <wml1293@outlook.de>
2023-02-28 09:14:40 +0100

    bf

--- scripts/execute_experiments.py
+++ scripts/execute_experiments.py
@@ -143,7 +143,7 @@ def main(repo_dir: str, experiment: str):
     if experiment == 'passive':
 
         # thresholds = [THRESH_TUNED, THRESH_PROGRESSIVE, THRESH_NOROUZZADEH]
-        thresholds = [THRESH_TUNED]
+        thresholds = [THRESH_PROGRESSIVE]
         sample_sizes: Dict = {}
         details_ins_test: Dict = {}
         details_ins_val: Dict = {}
