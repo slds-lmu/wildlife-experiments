21915e1b5193d4878530e36e07da069a949d0276
Lisa Wimmer <wml1293@outlook.de>
2023-03-13 08:32:36 +0100

    new runs

--- scripts/execute_tuning.py
+++ scripts/execute_tuning.py
@@ -59,9 +59,9 @@ def main(repo_dir: str, random_seed: int):
 
     # Define search grid
     search_space: Dict = {
-        'model_backbone': ['xception', 'densenet121', 'inception_resnet_v2'],
+        'model_backbone': ['densenet121'],  # ['xception', 'densenet121', 'inception_resnet_v2'],
         'finetune_layers': [0.25],  # [0, 0.05, 0.25, 0.5],
-        'md_conf': np.arange(0.1, 1, 0.2).round(2).tolist()
+        'md_conf': [0.5, 0.7, 0.9]  # np.arange(0.1, 1, 0.2).round(2).tolist()
     }
     search_grid = list(product_dict(**search_space))
 
