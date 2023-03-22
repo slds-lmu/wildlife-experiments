5b1be7a2c2c5d0cabc45f4de73d50c9dc087f251
Lisa Wimmer <wml1293@outlook.de>
2023-03-22 15:18:36 +0100

    analysis

--- scripts/execute_experiments.py
+++ scripts/execute_experiments.py
@@ -125,7 +125,7 @@ def main(repo_dir: str, experiment: str, random_seed: int, acq_criterion: str):
 
     if experiment == 'passive':
 
-        thresholds = [0.9]
+        thresholds = [0.]
         # thresholds = np.arange(0.1, 1, 0.2).round(2).tolist()
         # details_ins_test: Dict = {}
         # details_ins_val: Dict = {}
