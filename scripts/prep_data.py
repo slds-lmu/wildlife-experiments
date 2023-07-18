2bf0168a68d6f29a34c0af00cacc5743e01cbd51
Lisa Wimmer <wml1293@outlook.de>
2023-07-18 07:41:51 +0200

    bf

--- scripts/prep_data.py
+++ scripts/prep_data.py
@@ -19,47 +19,6 @@ from wildlifeml.utils.io import (
 from wildlifeml.utils.misc import flatten_list
 from utils import seed_everything
 
-# STATIONS_IS: Final[List] = [
-#     '8235_For',
-#     '5838_2For',
-#     '6225_2For',
-#     '7935_2_F',
-#     '5924_3For',
-#     '8229_2_F',
-#     '6027_3For',
-#     '6032_4For',
-#     '5636_4For',
-#     '7545_2For',
-#     '6131_1For',
-#     '6533_4For',
-#     '5923_3For',
-#     '6137_4For',
-#     '5938_3For',
-#     '8130_2_F',
-#     '5837_4For',
-#     '6234_2For'
-# ]
-# STATIONS_OOS: Final[List] = [
-#     '7143_4For',
-#     '7446_1For',
-#     '6035_3For',
-#     '8131_1_F',
-#     '6934_For',
-#     '7837_4_For',
-#     '8141_2For',
-#     '5933_4_For',
-#     '7544_2For',
-#     '8243_1For',
-#     '7332_For',
-#     '5728_2Fb',
-#     '6231_4For',
-#     '8030_1_F',
-#     '7730_2For',
-#     '5737_3_For',
-#     '6122_4For',
-#     '6034_2For',
-#     '5728_2Fa'
-# ]
 
 @click.command()
 @click.option(
@@ -142,13 +101,18 @@ def main(repo_dir: str, random_seed: int):
     )
 
     # Restructure station dict to match stratified-splitting arguments
-    stations = list(set(station_dict.values()))
-    station_dict = {k: {'station': v} for k, v in station_dict.items()}
+    station_dict_mod = {k: {'station': v} for k, v in station_dict.items()}
     # Define in-sample & out-of-sample keys according to camera stations
-    STATIONS_IS = list(random.sample(stations, math.ceil(0.5 * len(stations))))
-    STATIONS_OOS = list(set(stations) - set(STATIONS_IS))
-    keys_is = [k for k in all_keys if station_dict[k]['station'] in STATIONS_IS]
-    keys_oos = [k for k in all_keys if station_dict[k]['station'] in STATIONS_OOS]
+    if os.path.exists(os.path.join(cfg['data_dir'], 'stations_split.json')):
+        stations = load_json(os.path.join(cfg['data_dir'], 'stations_split.json'))
+        stations_is = stations['STATIONS_IS']
+        stations_oos = stations['STATIONS_OOS']
+    else:
+        stations = list(set(station_dict.items()))
+        stations_is = list(random.sample(stations, math.ceil(0.5 * len(stations))))
+        stations_oos = list(set(stations) - set(stations_is))
+    keys_is = [k for k in all_keys if station_dict_mod[k]['station'] in stations_is]
+    keys_oos = [k for k in all_keys if station_dict_mod[k]['station'] in stations_oos]
 
     # Split keys into train/val/test (only two-way for in-sample bc splitting is done
     # later according to chosen MD threshold)
@@ -156,19 +120,19 @@ def main(repo_dir: str, random_seed: int):
     keys_is_train, keys_is_val, keys_is_test = do_stratified_splitting(
         img_keys=keys_is,
         splits=cfg['splits'],
-        meta_dict=station_dict,
+        meta_dict=station_dict_mod,
         random_state=random_seed,
     )
     keys_oos_train, keys_oos_val, keys_oos_test = do_stratified_splitting(
         img_keys=keys_oos,
         splits=cfg['splits'],
-        meta_dict=station_dict,
+        meta_dict=station_dict_mod,
         random_state=random_seed,
     )
     keys_train, _, keys_val = do_stratified_splitting(
         img_keys=all_keys,
-        splits=(0.7/0.85, 0.,0.15/0.85),
-        meta_dict=station_dict,
+        splits=(0.7/0.85, 0., 0.15/0.85),
+        meta_dict=station_dict_mod,
         random_state=random_seed,
     )
 
