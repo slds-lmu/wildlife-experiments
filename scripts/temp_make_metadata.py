8f0e56e5068fb348b047515d8df9cd77a2487410
Lisa Wimmer <wml1293@outlook.de>
2023-07-18 09:13:12 +0200

    add label map

--- scripts/temp_make_metadata.py
+++ scripts/temp_make_metadata.py
@@ -1,7 +1,7 @@
 import click
 import os
 
-from wildlifeml.utils.io import load_json, save_as_csv
+from wildlifeml.utils.io import load_json, save_as_csv, save_as_json
 
 
 @click.command()
@@ -9,11 +9,11 @@ from wildlifeml.utils.io import load_json, save_as_csv
     '--md_file', '-f', help='Path to MD file.', required=True
 )
 @click.option(
-    '--repo_dir', '-p', help='Your personal path to this repo.', required=True
+    '--data_dir', '-p', help='Your personal path to this dir.', required=True
 )
-def main(md_file: str, repo_dir: str):
+def main(data_dir: str):
 
-    meta_data = load_json(md_file)
+    meta_data = load_json(os.path.join(data_dir, 'channel_islands_camera_traps.json'))
     station_dict = {x['id']: x['location'] for x in meta_data['images']}
     label_dict = {x['image_id']: x['category_id'] for x in meta_data['annotations']}
     meta_dict = {
@@ -22,9 +22,11 @@ def main(md_file: str, repo_dir: str):
     }
     save_as_csv(
         [(f'{k}.jpg', v['label'], v['station']) for k, v in meta_dict.items()],
-        os.path.join(repo_dir, 'data', 'metadata_channel_islands.csv'),
+        os.path.join(data_dir, 'metadata.csv'),
         header=['orig_name', 'true_class', 'station']
     )
+    label_map = {x['id']: x['name'] for x in meta_data['categories']}
+    save_as_json(label_map, os.path.join(data_dir, 'label_map_names.json'))
 
 
 if __name__ == '__main__':
