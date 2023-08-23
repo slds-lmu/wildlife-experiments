3ee6d7faea6ee97d728f829462fb777f3280f704
lisa-wm <wml1293@outlook.de>
2023-08-23 10:41:18 +0200

    pred interface

--- scripts/pred_final_model.py
+++ scripts/pred_final_model.py
@@ -8,6 +8,7 @@ from wildlifeml import WildlifeDataset
 from wildlifeml.data import BBoxMapper
 from wildlifeml.training.evaluator import Evaluator
 from wildlifeml.utils.io import load_json
+from wildlifeml.utils.misc import flatten_list
 
 IMG_DIR: Final[str] = '/common/bothmannl/wildlife_images/usecase2/original_images/'
 PROJ_DIR: Final[str] = '/home/wimmerl/projects/wildlife-experiments/'
@@ -18,7 +19,7 @@ MODEL_PATH: Final[str] = PROJ_DIR + 'data/bavaria/md5/final_ckpt/ckpt_final.hdf5
 THRESHOLD: Final[float] = 0.1
 KEYS: Final[List] = ['10000_I_00249b.JPG', '10001_I_00058a.JPG', '10003_I_00226c.JPG']
 BATCH_SIZE: Final[int] = 64
-NUM_CLASSES: Final[int] = 7
+NUM_CLASSES: Final[int] = 8
 EMPTY_CLASS_ID: Final[int] = 0
 
 
@@ -27,7 +28,7 @@ def main():
     mapper = BBoxMapper(DETECTOR_FILE_PATH)
     key_map = mapper.get_keymap()
     dataset = WildlifeDataset(
-        keys=[key_map[k] for k in KEYS],
+        keys=flatten_list([key_map[k] for k in KEYS]),
         image_dir=IMG_DIR,
         detector_file_path=DETECTOR_FILE_PATH,
         bbox_map=load_json(BBOX_PATH),
@@ -51,8 +52,8 @@ def main():
     )
     evaluator.evaluate(model)
     os.makedirs(PRED_DIR, exist_ok=True)
-    evaluator.save_predictions(file_path=PRED_DIR + 'preds_img.csv', img_level=True)
-    evaluator.save_predictions(file_path=PRED_DIR + 'preds_bbox.csv', img_level=False)
+    evaluator.save_predictions(filepath=PRED_DIR + 'preds_img.csv', img_level=True)
+    evaluator.save_predictions(filepath=PRED_DIR + 'preds_bbox.csv', img_level=False)
 
 
 if __name__ == '__main__':
